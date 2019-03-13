4db916ed9a41d9a023107e9759e94138f4af0bb9
flee <1742489089@qq.com>
2019-03-13 21:43:34 +0800

    commit num7

--- ReverseInteger.go
+++ ReverseInteger.go
@@ -3,12 +3,17 @@ import (
 	"fmt"
 )
 func reverse(x int) int {
+	max := int(^uint32(0) >> 1)
+	min := ^max
 	result := 0
 	current := 0
     for x!=0 {
 		current = x%10
 		result = result * 10 + current
 		x = x/10
+	}
+	if(result > max || result < min){
+		return 0
 	}
 	return result
 }
