28b17d8e4d81fd72c6de340dfc48c0c579d7f86f
flee <1742489089@qq.com>
2019-03-20 22:39:51 +0800

    commit num19

---
+++ num19.go
@@ -0,0 +1,25 @@
+package main
+
+ type ListNode struct {
+	Val int
+	Next *ListNode
+ }
+ func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := new(ListNode)
+	dummy.Next = head
+	first := dummy
+	second := dummy
+	for i:=1; i<=n+1; i++ {
+		first = first.Next
+	}
+	for first!=nil{
+		first = first.Next
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
+
+func main(){
+
+}
---
+++ num19.java
@@ -0,0 +1,23 @@
+
+ public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
+class num19{
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        ListNode first = dummy;
+        ListNode second = dummy;
+        dummy.next = head;
+        for(int i=1; i<=n+1; i++){
+            first = first.next;
+        }
+        while(first!=null){
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
