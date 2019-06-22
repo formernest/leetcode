8b836365de4c0bcd1ba265f323ac03ccd5d44a93
flee <1742489089@qq.com>
2019-06-22 14:37:09 +0800

    add 102,103

---
+++ num102.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+ }
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) return new ArrayList<List<Integer>>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        q.add(root);
+        while(q.size()>0){
+            int size = q.size();
+            List<Integer> interlist = new ArrayList<Integer>();
+            for(int i=0; i<size; i++){
+                TreeNode front = q.poll();
+                interlist.add(front.val);
+                if(front.left != null){
+                    q.add(front.left);
+                }
+                if(front.right != null){
+                    q.add(front.right);
+                }
+            }
+            list.add(interlist);
+        }
+        
+        return list;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
