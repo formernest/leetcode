f29e85f08120cbaf1c20a4cc9024cb4df693376c
flee <1742489089@qq.com>
2019-06-23 22:06:09 +0800

    add 110,690,877

---
+++ num110.java
@@ -0,0 +1,23 @@
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+ }
+class Solution {
+    boolean isbalanced = true;
+    public boolean isBalanced(TreeNode root) {
+        getDepth(root);
+        return isbalanced;
+    }
+    public int getDepth(TreeNode root){
+        if(root == null) return 0;
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if(Math.abs(left - right) > 1){
+            isbalanced = false;
+        }
+        return right > left? right+1 : left+1;
+    }
+}
---
+++ num690.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.HashMap;
+
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Employee emp = null;
+        int sum = 0;
+        HashMap<Integer, Employee> map = new HashMap<Integer, Employee>();
+        for(Employee e: employees){
+            map.put(e.id, e);
+        }
+        // 广度优先算法遍历
+        ArrayDeque<Employee> queue = new ArrayDeque<Employee>();
+        queue.addLast(map.get(id));
+        while(!queue.isEmpty()){
+            emp = queue.removeFirst();
+            sum += emp.importance;
+            for(int i:emp.subordinates){
+                queue.addLast(map.get(i));
+            }
+        }
+        return sum;
+    }
+}
