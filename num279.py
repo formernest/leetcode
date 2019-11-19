a46685923845d65a2b44dae807f3139cc7584b89
flee <1742489089@qq.com>
2019-11-19 23:10:19 +0800

    add num279

--- num279.py
+++ num279.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        dp = [float('inf') for _ in range(n+1)]
+        dp[0] = 0
+        for i in range(0, n+1):
+            j = 1
+            while i + j * j <= n:
+                dp[i + j * j] = min(dp[i] + 1, dp[i+j*j])
+                j += 1
+        return dp[n]
+
+if __name__ == "__main__":
+    solution = Solution()
+    n = 13
+    print(solution.numSquares(n))
